d1aa4358af2573dee15dd1d386de3b60da0b4021
KenAJoh <ken.aleksander.johansen@nav.no>
2020-09-21 08:59:22 +0200

    Fjernet transpilert lib filer

--- shared/lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-export { default as GithubApi } from './types/githubApi';
-export { default as CommitData } from './types/commits';
-export { default as RepoResult } from './types/repoResult';
-export { default as NameFilter } from './types/nameFilter';
-export { default as PackFilter } from './types/packFilter';
-export { default as RootFilter } from './types/rootFilter';
-export { default as VersionLimit } from './types/versionLimit';
-export { ServerResults, History, Stat } from './types/data';
-export { default as Repo } from './types/repo';
